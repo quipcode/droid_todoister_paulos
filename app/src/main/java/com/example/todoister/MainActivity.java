73a1fec9073f908a39a852ba3b86d3399b12a4d0
quipcode <abdulkarim8ahmed@gmail.com>
2021-04-19 08:12:09 -0700

    completish project

--- app/src/main/java/com/example/todoister/MainActivity.java
+++ app/src/main/java/com/example/todoister/MainActivity.java
@@ -3,18 +3,41 @@ package com.example.todoister;
 
 import android.os.Bundle;
 
+import com.example.todoister.adapter.OnTodoClickListener;
+import com.example.todoister.adapter.RecyclerViewAdapter;
+import com.example.todoister.model.Priority;
+import com.example.todoister.model.SharedViewModel;
+import com.example.todoister.model.Task;
+import com.example.todoister.model.TaskViewModel;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Calendar;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity  implements OnTodoClickListener {
+
+    private TaskViewModel taskViewModel;
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter recyclerViewAdapter;
+    private int counter;
+    BottomSheetFragment bottomSheetFragment;
+    private SharedViewModel sharedViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +45,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        counter = 0;
+        bottomSheetFragment = new BottomSheetFragment();
+        ConstraintLayout constraintLayout = findViewById(R.id.bottomSheet);
+        BottomSheetBehavior<ConstraintLayout> bottomSheetBehavior = BottomSheetBehavior.from(constraintLayout);
+        bottomSheetBehavior.setPeekHeight(BottomSheetBehavior.STATE_HIDDEN);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+        taskViewModel = new ViewModelProvider.AndroidViewModelFactory(MainActivity.this.getApplication()).create(TaskViewModel.class);
+
+        sharedViewModel = new ViewModelProvider(this).get(SharedViewModel.class);
+
+        taskViewModel.getAllTasks().observe(this, tasks -> {{
+            recyclerViewAdapter = new RecyclerViewAdapter(tasks, this);
+            recyclerView.setAdapter(recyclerViewAdapter);
+        }
+            
         });
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+//            Task task = new Task("Task " + counter++, Priority.MEDIUM,  Calendar.getInstance().getTime(), Calendar.getInstance().getTime(), false);
+//            TaskViewModel.insert(task);
+            showBottomSheetDialog();
+        });
+    }
+
+    private void showBottomSheetDialog() {
+        bottomSheetFragment.show(getSupportFragmentManager(), bottomSheetFragment.getTag());
     }
 
     @Override
@@ -53,5 +96,22 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onTodoClick(Task task) {
+
+//        Log.d("TAG clicked", "onTodoClick:  " + task.getTask());
+        sharedViewModel.selectItem(task);
+        sharedViewModel.setIsEdit(true);
+        Log.d("Mainster", "onTodoClick: main isedit: " + sharedViewModel.getIsEdit());
+        showBottomSheetDialog();
+    }
+
+    @Override
+    public void onTodoRadioButtonClick(Task task) {
+//        Log.d("TAG clicked", "onTodoClick:  " + task.getTask());
+        TaskViewModel.delete(task);
+        recyclerViewAdapter.notifyDataSetChanged();
     }
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,13 @@
 *Todoister clone from [Paulo's](https://twitter.com/buildappswithme?lang=en) [The Comprehensive 2021 Android Development Masterclass](https://www.udemy.com/course/android-development-java-android-studio-masterclass/)*
+
+### Features included:
+* Navigating between activities/fragments
+* CRUD operation to RoomDatabase
+* Utilizing RecyclerView to list out todo's
+* Utilizing taskViewModel and viewModel to handle and pass data around activities/fragments
+
+
+## Images
+
+![RecyclerView](/images/todo.png "MainActivity/RecyclerView")
+![BottomSheetActivity](/images/todoister.png "BottomSheetActivity/Calendar + Priority")
